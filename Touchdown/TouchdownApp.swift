96aa25f4102ce5613ab50275ba1a0a3c3bb0ad8a
NT <usedforfunny@gmail.com>
2022-04-15 10:52:14 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Alexandre Talatinian on 15/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
