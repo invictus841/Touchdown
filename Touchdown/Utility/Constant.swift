1bbb64a75af251c897fee74070de9e98462b1302
NT <usedforfunny@gmail.com>
2022-04-15 11:36:36 +0200

    Creation of FooterView Creation of a constants file(assets, colors, etc) file in utility folder

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,24 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Alexandre Talatinian on 15/04/2022.
+//
+
+import SwiftUI
+
+// DATA
+
+
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
