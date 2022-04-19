58030fccf0c3514155956bffc082c76af1c71ea0
NT <usedforfunny@gmail.com>
2022-04-19 10:42:20 +0200

    Added BrandModel Added BrandItemView and BrandGridView
    Horizontal grid for the brands

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Alexandre Talatinian on 19/04/2022.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
